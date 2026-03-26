17647fe66d6d679b9b577c288566a66b3f787233
hemalchavda123 <hemalchavda699@gmail.com>
2026-03-26 17:26:49 +0530

    fixed an import

--- frontend/src/signup2/Signup2.tsx
+++ frontend/src/signup2/Signup2.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import React, { useState, useEffect } from "react"
+import React, { useState, useEffect, JSX } from "react"
 import { useRouter } from "next/navigation"
 import Navbar from "@/components/Navbar"
 import Footer from "@/components/Footer"
