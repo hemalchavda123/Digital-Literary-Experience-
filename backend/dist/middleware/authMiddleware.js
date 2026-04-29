bf72cba630f426a58b79cb04d38af43795e85e80
hemalchavda123 <hemalchavda699@gmail.com>
2026-04-29 07:41:44 +0530

    chore: ignore dist folder

--- backend/dist/middleware/authMiddleware.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.authMiddleware = void 0;
-const authMiddleware = (req, res, next) => {
-    // Mock authentication for now
-    req.user = { id: 'demo-user' };
-    next();
-};
-exports.authMiddleware = authMiddleware;
